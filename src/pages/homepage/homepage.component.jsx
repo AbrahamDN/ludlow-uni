06ec77f1991c61e92803eb600aca1b3404e74fde
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-08-18 18:43:11 +0100

    initial commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This is working</p>
+    </div>
+  );
+};
+
+export default Homepage;
