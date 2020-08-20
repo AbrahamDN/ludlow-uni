85e7dd6cf8002d2822d28c15dcdbc0a78d11f217
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-08-20 04:29:38 +0100

    Added homepage header

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import './homepage.styles.scss';
 
-const Homepage = () => {
+import badge from '../../svg/logo-badge.svg';
+
+const Homepage = ({ isMobile }) => {
   return (
     <div className='homepage'>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
-      <p>Paragraph</p>
+      <header className='header'>
+        <div className='header-content'>
+          <div className='header-content-wrap'>
+            <h1>Student Life</h1>
+            <h2>
+              There is so much going on at Ludlow University, both on and off
+              campus.
+            </h2>
+            <div className='info'>
+              <h3>Lisa Smith</h3>
+              <p>Student, BSc (Hons) Computer Games Design</p>
+            </div>
+            <button>View student stories</button>
+          </div>
+        </div>
+        <div className={`header-hero ${isMobile ? 'first_grid' : ''}`}>
+          <div
+            className='hero-content'
+            style={{ backgroundImage: `url('/assets/jpg/hero-image.jpg')` }}>
+            <img src={badge} alt='Logo badge' />
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
 
-export default Homepage;
+const mapStateToProps = state => ({
+  isMobile: state.isMobile,
+});
+export default connect(mapStateToProps)(Homepage);
