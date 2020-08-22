00ab69ee70f97640e2fbf39224cc870ef29fddd5
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-08-22 10:00:14 +0100

    fixed footer err

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.scss';
 
 import Navbar from './components/navbar/navbar.component';
 import Homepage from './pages/homepage/homepage.component';
-import Footer from './components/footer/footer.componentonent';
+import Footer from './components/footer/footer.component';
 
 const App = ({ dispatchIsMobile, dispatchIsNotMobile }) => {
   const [windowSize, setWindowSize] = useState(window.innerWidth);
